46782d508c729d5dfdd5abedc27905180a8d6055
Tarun Ramesh <cpatarun@gmail.com>
2019-01-22 16:26:19 +0800

    finally

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #! /usr/bin/env node
 
 const fs = require("fs");
-const pm2 = require("pm2");
 const shelljs = require("shelljs");
+const path = require("path");
 
 const command = process.argv[2];
+const user = process.argv[3];
 
 if (command === "init") {
-
   fs.writeFileSync("./config.json", JSON.stringify({
     copies: 1,
     api_url: "https://api.cloudwaitress-test.com",
@@ -16,122 +16,20 @@ if (command === "init") {
       "FK80"
     ]
   }, null, 2));
-
   console.log("CONFIG FILE CREATED");
+}
+else if (command === "startup") {
+  const script = path.join(__dirname, "index.sh");
+  const cwd = process.cwd();
+  shelljs.exec(`${script} startup ${cwd} ${__dirname} ${user}`, function(code, stdout, stderr) {
+    // console.log('Exit code:', code);
+  });
 
 }
 else if (command === "start") {
-
-  pm2.connect(function(err) {
-
-    if (err) {
-      console.error(err);
-      process.exit(2)
-    }
-
-    if (!fs.existsSync("./logs")) {
-      fs.mkdirSync("./logs");
-    }
-
-    const now = Date.now();
-
-    fs.readdir("./logs", (err, files) => {
-      files.forEach(file => {
-        const created = parseInt(file, 10);
-        if (now > (created + (1000 * 60 * 60 * 24 * 5))) {
-          const f = `./logs/${file}`;
-          console.log("CLEANING OLD LOG FILE", f);
-          fs.unlinkSync(f);
-        }
-      });
-    });
-
-    pm2.start({
-      script: __dirname + "/index-service.js",
-      error: `./logs/${now}`,
-      output: `./logs/${now}`,
-      env: {
-        DEBUG: "ERROR,WARN,INFO,DEV,PRINT-SERVICE,PRINTER"
-      }
-    }, (err, apps) => {
-
-      console.log("STARTED");
-
-      pm2.disconnect();
-
-      shelljs.exec('tail -f logs/*', function(code, stdout, stderr) {
-        console.log('Exit code:', code);
-        console.log('Program output:', stdout);
-        console.log('Program stderr:', stderr);
-      });
-
-      if (err) { throw err }
-
-    })
-
-  });
-
-}
-else if (command === "log") {
-
-  shelljs.exec('tail -f logs/*', function(code, stdout, stderr) {
-    console.log('Exit code:', code);
-    console.log('Program output:', stdout);
-    console.log('Program stderr:', stderr);
-  });
-
-}
-else if (command === "stop") {
-  pm2.killDaemon(function(err) {
-    if (err) {
-      console.error(err);
-      process.exit(2)
-    }
-    else {
-      console.log("STOPPED");
-      pm2.disconnect();
-      process.exit(0);
-    }
-  });
-}
-else if (command === "reload") {
-  pm2.reload("all", function(err) {
-    if (err) {
-      console.error(err);
-      process.exit(2)
-    }
-    else {
-      console.log("RELOADED");
-      pm2.disconnect();
-      shelljs.exec('tail -f logs/*', function(code, stdout, stderr) {
-        console.log('Exit code:', code);
-        console.log('Program output:', stdout);
-        console.log('Program stderr:', stderr);
-      });
-    }
-  });
-}
-else if (command === "list") {
-  pm2.list(function(err, list) {
-    if (err) {
-      console.error(err);
-      process.exit(2)
-    }
-    if (list.length !== 1) {
-      console.log(list);
-    }
-    else {
-      const p = list[0];
-      console.log(`
-        PID: ${p.pid}, 
-        NAME: ${p.name}, 
-        CPU: ${p.monit.cpu} 
-        MEMORY: ${p.monit.memory / 1000000}mb
-        UPTIME: ${p.pm2_env.pm_uptime}
-      `)
-    }
-    pm2.disconnect();
-  });
+  process.env.DEBUG = "ERROR,WARN,INFO,DEV,PRINT-SERVICE,PRINTER";
+  const service = path.join(__dirname, "index-service.js");
+  require(service);
 }
 else {
   console.log(command);
---
+++ index.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+COMMAND=$1
+CWD_DIR=$2
+SCRIPT_DIR=$3
+USR=$4
+
+if [[ ${COMMAND} == "startup" ]]; then
+
+cat <<- EOF > ${CWD_DIR}/startup.sh
+#!/bin/bash
+sudo cupsctl --remote-admin --remote-any --share-printers
+sudo service cups restart
+sudo rfcomm bind /dev/rfcomm0 02:1F:32:F9:12:25 1
+sudo touch ${CWD_DIR}/startup-success
+EOF
+
+sudo cat <<- EOF > /etc/systemd/system/cw-print-startup.service
+[Unit]
+Description=Headless Printing Startup
+After=bluetooth.service
+Requires=bluetooth.service
+
+[Service]
+User=root
+ExecStart=${CWD_DIR}/startup.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo cat <<- EOF > /etc/systemd/system/cw-print.service
+[Unit]
+Description=Headless Printing
+After=network.target
+Wants=cw-print-startup.service
+
+[Service]
+User=${USR}
+WorkingDirectory=${CWD_DIR}
+ExecStart=/usr/bin/node ${SCRIPT_DIR}/index.js start
+Restart=always
+RestartSec=500ms
+StartLimitInterval=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo chmod +x ${CWD_DIR}/startup.sh
+
+sudo loginctl enable-linger ${USR}
+
+sudo systemctl disable cw-print-startup.service
+sudo systemctl disable cw-print.service
+
+sudo systemctl enable cw-print-startup.service
+sudo systemctl enable cw-print.service
+
+sudo systemctl daemon-reload
+
+fi
+
